1230943f36dcce7e39164303e94a9d0b3a1753ed
@THIYAGUSRI <thiyagusrinivasan09@gmail.com>
2026-02-17 22:00:00 +0530

    server page

--- server.js
+++ server.js
@@ -2013,11 +2013,11 @@ app.get('/', (req, res) => {
 app.use(handleMulterError);
 
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'FrontEnd/dist')));
 
 // Catch-all: send index.html for any non-API route (React Router needs this)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'FrontEnd/dist/index.html'));
 });
 
 app.listen(PORT, () => {
